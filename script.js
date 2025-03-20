f1ed4c2068b34dec532c23e2ce65a1406aedd922
Cameron Jones <jones_cameron@student.mahoningctc.com>
2025-03-20 14:03:02 -0400

    done

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.getElementById('feedbackForm').addEventListener('submit', function(e) {
     e.preventDefault();
  
- 
     const name = document.getElementById('name').value;
     const age = parseInt(document.getElementById('age').value);
     const phone = document.getElementById('phone').value;
@@ -12,10 +11,56 @@ document.getElementById('feedbackForm').addEventListener('submit', function(e) {
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
  
+    console.log("Age entered:", age);
  
-    //! This part is the challenge. You need to add validation for the form below. If any of the fields are invalid, alert the user and return from the function.
-    // Add Validation for the form below. If any of the fields are invalid, alert the user and return from the function.
- 
+    
+    if (isNaN(age) || age < 18) {
+        alert('You must be at least 18 years old to submit this form.');
+        return; 
+    }
+
+
+    if (zip.length !== 5 || isNaN(zip)) {
+        alert('Zip code must be 5 digits.');
+        return;
+    }
+
+
+    if (phone.length !== 9 || isNaN(phone)) {
+        alert('Phone number must be 9 digits.');
+        return;
+    }
+
+
+    if (name.trim() === "") {
+        alert('Name cannot be blank.');
+        return; 
+    }
+
+    if (street.trim() === "") {
+        alert('Street name cannot be blank.');
+        return; 
+    }
+
+    if (city.trim() === "") {
+        alert('city name cannot be blank.');
+        return; 
+    }
+
+    if (state.trim() === "") {
+        alert('State name cannot be blank.');
+        return; 
+    }
+
+    if (message.trim() === "") {
+        alert('Feedback message cannot be blank.');
+        return; 
+    }
+    
+    if (email.trim() === "") {
+        alert('Email cannot be blank.');
+        return; 
+    }
  
     alert('Feedback submitted successfully!');
  });
